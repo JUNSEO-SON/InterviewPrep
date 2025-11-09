d041ea78addb37fc73bd1a66db0a76b4efb57890
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-09 15:50:55 +0900

    Title: 행복한 수열의 개수, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

--- 251109/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251109/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,12 +5,19 @@ answer=0
 for row in range(n): #행별로 행복 수열 찾기
     lst=[]
     count=1
+    max_count=0
     for col in range(n):
         if lst and grid[row][col]==lst[-1]:
             count+=1
+
+        if lst and grid[row][col]!=lst[-1]:
+            max_count=max(max_count,count)
+            count=1
+
         lst.append(grid[row][col])
 
-
+    count=max(max_count,count)
+    
     if count>=m:
         answer+=1
 
@@ -18,11 +25,18 @@ for row in range(n): #행별로 행복 수열 찾기
 for col in range(n): #열별로 행복 수열 찾기
     lst=[]
     count=1
+    max_count=0
     for row in range(n):
         if lst and grid[row][col]==lst[-1]:
             count+=1
+
+        if lst and grid[row][col]!=lst[-1]:
+            max_count=max(max_count,count)
+            count=1
+
         lst.append(grid[row][col])
     
+    count=max(max_count,count)
 
     if count>=m:
         answer+=1

c31cd99ea3ab90323d98fc37c1a11d275c25e04c
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-15 03:31:57 +0900

    Title: 떨어지는 1자 블록, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251115/떨어지는 1자 블록/falling-horizontal-block.py
+++ 251115/떨어지는 1자 블록/falling-horizontal-block.py
@@ -9,15 +9,17 @@ location=0
 for col in range(k,k+m):
     for row in range(n):
         if grid[row][col-1]==1:
-            height=min(height,row)
+            if min(height,row)!=height:
+                height=min(height,row)
+                location=col-1
 
-count=0
-for col in range(n):
-    if grid[height-1][col]==0:
-        grid[height-1][col]=1
-        count+=1
-    if count==m:
-        break
+target_height=height-1
+
+
+#location부터 m개 -> 
+for col in range(location,location+m):
+    grid[target_height][col]=1
+
 
 for row in grid:
     print(*row)

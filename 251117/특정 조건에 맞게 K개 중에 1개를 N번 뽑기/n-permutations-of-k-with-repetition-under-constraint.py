d86147b0f241960880e6c0895ef5b572f00340d1
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-17 03:17:53 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251117/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,28 @@
+K, N = map(int, input().split())
+
+# Please write your code here.
+ans=[]
+lst=[]
+
+def choose(count):
+    if len(lst)==N:
+        ans.append(lst.copy())
+        return
+
+    for i in range(1,K+1):
+        if lst and lst[-1]==i:
+            new_count=count+1
+        else:
+            new_count=1
+        
+        if count<3:
+            lst.append(i)
+            choose(new_count)
+            lst.pop()
+        
+
+choose(1)
+print(ans)
+
+
+

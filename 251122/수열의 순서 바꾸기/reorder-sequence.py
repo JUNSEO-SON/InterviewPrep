913b2e224b09a2bc88be8535596f2fa76fda0f92
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-22 16:44:38 +0900

    Title: 수열의 순서 바꾸기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251122/수열의 순서 바꾸기/reorder-sequence.py
+++ 251122/수열의 순서 바꾸기/reorder-sequence.py
@@ -4,7 +4,7 @@ sequence = list(map(int, input().split()))
 # Please write your code here.
 
 l=len(sequence)
-for i in range(l-1,0,-1):
+for i in range(l-1,-1,-1):
     if sequence[i]<sequence[i-1]:
         break
 

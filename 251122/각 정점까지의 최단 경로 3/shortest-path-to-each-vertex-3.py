92ea2fb0706af7e0639982b0e6a0f078a4d06fe1
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-22 19:14:53 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251122/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -0,0 +1,36 @@
+from heapq import heappop,heappush
+n, m = map(int, input().split())
+
+graph=[[] for _ in range(n+1)]
+
+for _ in range(m):
+    u,v,w = map(int, input().split())
+    graph[u].append((v,w))
+
+# Please write your code here.
+
+def dijkstra(graph,start):
+    l=len(graph)
+    distances=[float('inf') for _ in range(l)]
+    distances[start]=0
+
+    pq=[]
+    heappush(pq,(start,0))
+
+    while pq:
+        curr_node,curr_dist=heappop(pq)
+
+        if distances[curr_node]<curr_dist:
+            continue
+    
+        for n,w in graph[curr_node]:
+            new_dist=curr_dist+w
+            if new_dist<distances[n]:
+                distances[n]=new_dist
+                heappush(pq,(n,new_dist)) 
+    
+    return distances
+
+
+distances=dijkstra(graph,1)
+print(*distances[2:],sep='\n')

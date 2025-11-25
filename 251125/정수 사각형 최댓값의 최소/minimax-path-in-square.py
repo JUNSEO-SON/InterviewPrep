9bc87242cc0c0941b5859b32c63ef268306c3a85
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-11-25 17:03:45 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 69ms, Memory: 18MB, Status: Passed - Codetree

--- 251125/정수 사각형 최댓값의 최소/minimax-path-in-square.py
+++ 251125/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -9,14 +9,15 @@ dp=[[0 for _ in range(n)] for _ in range(n)]
 for i in range(len(dp)):
     for j in range(len(dp)):
         if j==0:
-            dp[i][j]=grid[i][j]
+            dp[i][j]=max(grid[i][j],dp[i-1][j])
         if i==0:
-            dp[i][j]=grid[i][j]
+            dp[i][j]=max(grid[i][j],dp[i][j-1])
         else:
             continue
 
 for i in range(1,len(dp)):
     for j in range(1,len(dp)):
         dp[i][j]=max(min(dp[i-1][j],dp[i][j-1]),grid[i][j])
+
 
 print(dp[n-1][n-1])

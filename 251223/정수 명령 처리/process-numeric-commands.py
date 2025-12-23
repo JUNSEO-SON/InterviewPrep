c8935191a593f1b5699767f80fb05d5554959f96
JUNSEO-SON <wnstj480@unist.ac.kr>
2025-12-23 20:23:00 +0900

    Title: 정수 명령 처리, Time: 179ms, Memory: 24MB, Status: Passed - Codetree

--- 251223/정수 명령 처리/process-numeric-commands.py
+++ 251223/정수 명령 처리/process-numeric-commands.py
@@ -1,15 +1,20 @@
 N = int(input())
-command = []
-value = []
-
+stack=[]
 for _ in range(N):
-    line = input().split()
-    command.append(line[0])
-    if line[0] == "push":
-        value.append(int(line[1]))
+    command=input().split()
+
+    if len(command)==2:
+        stack.append(command[1])
+
     else:
-        value.append(0)
-
-# Please write your code here.
-
-print(command,'\n',value)
+        if command[0]=='pop':
+            print(stack.pop())
+        elif command[0]=='size':
+            print(len(stack))
+        elif command[0]=='empty':
+            if len(stack)==0:
+                print(1)
+            else:
+                print(0)
+        elif command[0]=='top':
+            print(stack[-1])
